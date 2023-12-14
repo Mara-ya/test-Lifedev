a67a7498687488091270f783315561131891f20b
Mara-ya <mariya18.99.08@gmail.com>
2023-12-14 19:36:51 +0200

    debugging basename path

--- src/components/App.js
+++ src/components/App.js
@@ -10,45 +10,47 @@ import { HelpPage } from 'pages/Help';
 
 const router = createBrowserRouter([
   {
-      path: '/test-lifedev/',
-      element: <Layout />,
-      children: [
-        {
-          path: 'dashboard',
-          element: <DashboardPage />,
-        },
-        {
-          path: 'product',
-          element: <ProductPage />,
-        },
-        {
-          index: true,
-          path: '',
-          element: <CustomersPage />,
-        },
-        {
-          path: 'customers',
-          element: <CustomersPage />,
-        },
-        {
-          path: 'income',
-          element: <IncomePage />,
-        },
-        {
-          path: 'promote',
-          element: <PromotePage />,
-        },
-        {
-          path: 'help',
-          element: <HelpPage />,
-        },
-        {
-          path: '*',
-          element: <Navigate to="/customers" replace />,
-        }
-      ],
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: 'dashboard',
+        element: <DashboardPage />,
+      },
+      {
+        path: 'product',
+        element: <ProductPage />,
+      },
+      {
+        index: true,
+        path: '',
+        element: <CustomersPage />,
+      },
+      {
+        path: 'customers',
+        element: <CustomersPage />,
+      },
+      {
+        path: 'income',
+        element: <IncomePage />,
+      },
+      {
+        path: 'promote',
+        element: <PromotePage />,
+      },
+      {
+        path: 'help',
+        element: <HelpPage />,
+      },
+      {
+        path: '*',
+        element: <Navigate to="/customers" replace />,
+      }
+    ],
   },
-]);
+],{
+  basename: "/test-lifedev",
+});
 
 export const App = () => {
   return (
